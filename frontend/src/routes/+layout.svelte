e7462f14d2c0c039fcc5a300f96e9bddc92cc5da
tora29 <nikumann.power@gmail.com>
2025-03-31 22:32:29 +0900

    Skelton, Lucide の追加、header, sidebar の追加、alias, runemode の設定を追加、サンプルページの削除

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import '../app.css';
+	let { children } = $props();
+	import Header from '$widgets/header/ui/header.svelte';
+  import Sidebar from '$widgets/sidebar/sidebar.svelte';
+</script>
+
+<div class="flex flex-col h-screen w-full">
+  <Header />
+  <div class="flex flex-1 overflow-hidden">
+    <Sidebar />
+    <main class="flex-1 overflow-auto p-4">
+      {@render children()}
+    </main>
+  </div>
+</div>
