331e53702e7ce594d0cababf7b3af3911104b257
Tora29 <pizamann2525@icloud.com>
2025-05-12 22:43:31 +0900

    本番環境用の.env が使用されるよう修正

--- frontend/src/shared/utils/apiConfigUtils.ts
+++ frontend/src/shared/utils/apiConfigUtils.ts
@@ -5,5 +5,7 @@
 // 本番環境かどうかを判定
 export const isProduction = import.meta.env.PUBLIC_ENV === 'production';
 
-// API基本URL
-export const API_BASE_URL = 'http://localhost:8000/v1';
+// 環境変数のPUBLIC_API_BASE_URLのみを使用する
+export const API_BASE_URL = import.meta.env.DEV 
+  ? 'http://localhost:8000/v1'
+  : import.meta.env.PUBLIC_API_BASE_URL;
